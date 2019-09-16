1402df1688fc07185e984d77d492b20fb09ae437
Matheus <matheus2000846@gmail.com>
2019-09-16 15:35:39 -0300

    Movido Pasta De Estudos para outro Repositório

--- ARQUIVOS - PARA - ESTUDOS/HTML5/codigos/cap_09/calcula.js
+++
@@ -1,3 +0,0 @@
-var r=0;
-for(i=0; i<=1e+8; i++) {r += i;} 
-postMessage(r);	
--- ARQUIVOS - PARA - ESTUDOS/HTML5/codigos/cap_09/exemplo3.js
+++
@@ -1,5 +0,0 @@
-onmessage = function(e) {
-	var mensagemRecebida = e.data;
-	var mensagemRetornada = 'O Worker acusa o recebimento da seguinte mensagem:<br><span class="dest">'+ mensagemRecebida +'</span>';
-	postMessage(MensagemRetornada);
-}
